fc667e492ce02915dd4a2a04f244d4492b8c65ab
Cosmos <guitarnerd777@gmail.com>
2016-03-22 12:57:26 -0600

    khv

--- sr/thing/DoNotExist.java
+++ sr/thing/DoNotExist.java
@@ -1,10 +1,7 @@
 package thing;
 
 public class DoNotExist {
-
 	public static void main(String[] args) {
 		System.out.println("I don't have a project. Therefore I do not have anything for this.");
-
 	}
-
 }
