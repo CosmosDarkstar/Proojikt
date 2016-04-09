cc2c38058fe3c0e14512b6f8d45a78317ddcc54c
Cosmos Darkstar <cosmosdarkstarbrony@gmail.com>
2016-04-09 12:54:28 -0600

    lkateb

--- sr/thing/Example7.java
+++ sr/thing/Example7.java
@@ -7,23 +7,25 @@ import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 
 import jaco.mp3.player.MP3Player;
+import jaco.mp3.player.plaf.MP3PlayerUICompact;
 
 public class Example7 {
+	static String[] names;
+	static int i = 0;
 
 	public static void main(String[] args) throws Exception {
 
-		// MP3Player.setDefaultUI(MP3PlayerUICompact.class);
+		MP3Player.setDefaultUI(MP3PlayerUICompact.class);
 
 		MP3Player player = new MP3Player();
 
 		player.setRepeat(true);
-
-		player.addToPlayList(new File("01 Hello (Metal Cover) [feat. Pete C.mp3"));
-		player.addToPlayList(new File("LMFAO - Shots.mp3"));
-		player.addToPlayList(new File("LMFAO - Yes.mp3"));
-		// player.addToPlayList(new URL("http://server.com/mp3s/test4.mp3"));
-
-		//
+		File in = new File("D:\\teh musics\\Feared-Synder-MP3\\Feared\\Synder\\");
+		File[] pile = in.listFiles();
+		for (File f : pile) {
+			player.addToPlayList(pile[i]);
+			i++;
+		}
 
 		player.setBorder(BorderFactory.createEmptyBorder(50, 100, 50, 100));
 
@@ -33,6 +35,16 @@ public class Example7 {
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+	}
+
+	public void listFilesForFolder(File folder) {
+		for (File fileEntry : folder.listFiles()) {
+			if (fileEntry.isDirectory()) {
+				listFilesForFolder(fileEntry);
+			} else {
+				System.out.println(fileEntry.getName());
+			}
+		}
 	}
 
 }
