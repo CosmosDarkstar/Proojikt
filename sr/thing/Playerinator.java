bf78384973138dc4f613b4ef138c874e1158684c
Cosmos Darkstar <cosmosdarkstarbrony@gmail.com>
2016-04-11 21:03:13 -0600

    all of my s**t's in bits.

--- sr/thing/Playerinator.java
+++ sr/thing/Playerinator.java
@@ -1,53 +1,49 @@
 package thing;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import jaco.mp3.player.MP3Player;
 
-public class Playerinator {
+public class Playerinator implements ActionListener {
+	private static ActionListener a = new Playerinator();
+	private static ActionListener b = new Playerinator();
+
+	private static MP3Player player = new MP3Player();
+
+	private static ArrayList<File> pill = new ArrayList<File>();
+	private static ArrayList<File> pil;
+
+	private static JFrame frame = new JFrame("MP3 Player");
+	private static BorderLayout lay = new BorderLayout();
+	private static JTextField vop = new JTextField();
+	private static JButton der = new JButton("Click when directory entered.");
+	private static JButton de = new JButton("Click here to move to player.");
+	private static JLabel as = new JLabel("Enter directory here");
+
+	private static String dirc;
 
 	public static void main(String[] args) throws Exception {
-
-		MP3Player player = new MP3Player();
 		player.setRepeat(true);
 
-		ArrayList<File> pill = new ArrayList<File>();
-		ArrayList<File> pil = listf("D:\\teh musics\\", pill);
-
-		for (File f : pil) {
-
-			player.addToPlayList(f);
-			System.out.println(f);
-
+		inputGUI();
+		while (!vop.getText().isEmpty()) {
+			dirc = vop.getText();
 		}
+		playlistAdd(dirc);
 
-		JFrame frame = new JFrame("MP3 Player");
-		BorderLayout lay = new BorderLayout();
-		JTextField vop = new JTextField();
-		frame.setSize(900, 200);
-		player.setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
-
-		lay.setHgap(10);
-		lay.setVgap(5);
-		frame.setLayout(lay);
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(player, BorderLayout.NORTH);
-		frame.add(new JLabel("Enter directory here"), BorderLayout.CENTER);
-		frame.add(vop, BorderLayout.SOUTH);
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
 	}
 
-	public static ArrayList<File> listf(String grak, ArrayList<File> files) {
+	private static ArrayList<File> listf(String grak, ArrayList<File> files) {
 		File floobl = new File(grak);
 
 		File[] fList = floobl.listFiles();
@@ -59,6 +55,85 @@ public class Playerinator {
 			}
 		}
 		return files;
+	}
+
+	private static void playlistAdd(String dirc) {
+		// if (!dirc.isEmpty()) {
+		pil = listf(dirc, pill);
+		for (File f : pil) {
+
+			player.addToPlayList(f);
+			System.out.println(f);
+
+		}
+		// }
+
+	}
+
+	private static void initialGUI() {
+		// frame.setSize(900, 200);
+		player.setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
+
+		lay.setHgap(10);
+		lay.setVgap(5);
+		frame.setLayout(lay);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private static void inputGUI() {
+		player.setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
+
+		lay.setHgap(10);
+		lay.setVgap(5);
+		frame.setLayout(lay);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(as, BorderLayout.NORTH);
+		frame.add(vop, BorderLayout.CENTER);
+		frame.add(der, BorderLayout.SOUTH);
+		der.addActionListener(a);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	private void playerGUI() {
+		frame.remove(as);
+		frame.remove(vop);
+		frame.remove(der);
+		frame.remove(de);
+		frame.add(player, BorderLayout.NORTH);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	private void stepGUI() {
+		frame.remove(as);
+		frame.remove(vop);
+		frame.remove(der);
+		frame.add(de, BorderLayout.NORTH);
+		de.addActionListener(b);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		buttonClicked(de, b);
+	}
+
+	void buttonClicked(JButton butt, ActionListener x) {
+		playerGUI();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// playlistAdd(dirc);
+		stepGUI();
+
+	}
+
+	public void actionPerformed(ActionEvent arg0, String f) {
+		playerGUI();
 	}
 
 }
